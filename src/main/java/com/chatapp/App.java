b4763a47cd9278fb38bbd8156d6b48863542ac6d
Priyansh Rathore <priyanshrathore123456@gmail.com>
2025-06-15 12:15:06 +0530

    icon rabit applied

--- src/main/java/com/chatapp/App.java
+++ src/main/java/com/chatapp/App.java
@@ -3,9 +3,11 @@ package com.chatapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
     @Override
@@ -14,6 +16,7 @@ public class App extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         scene.getStylesheets().add(getClass().getResource("/css/dark.css").toExternalForm());
         stage.setTitle("ChatApp - Login");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/rabit.jpg")));
         stage.setScene(scene);
         stage.show();
     }
